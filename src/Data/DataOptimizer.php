53eb227de1e94997289ea56c52bf0a35bd24e3c1
BMTmohammedtaha <bmt.mohammedtaha@gmail.com>
2023-09-25 17:21:26 +0200

    update applyRule

--- src/Data/DataOptimizer.php
+++ src/Data/DataOptimizer.php
@@ -86,43 +86,76 @@ class DataOptimizer
      * Apply a validation rule to a value and transform it accordingly.
      *
      * @param string $rule The validation rule.
+     * @param string $key The key to validate and transform.
      * @param mixed $value The value to validate and transform.
-     * @return mixed The transformed value.
+     * @return object The transformed value.
      */
-    public function applyRule(string $rule, $value)
+    public function applyRule(string $rule, string $key, $value)
     {
+
         if ($rule === 'json' && $this->isJson($value)) {
-            return json_decode($value);
+            $value = json_decode($value);
         }
 
         if ($rule === 'integer' && is_numeric($value)) {
-            return (int) $value;
+            $value = (int) $value;
         }
 
         if ($rule === 'float' && is_numeric($value)) {
-            return (float) $value;
+            $value = (float) $value;
         }
 
         if ($rule === 'string') {
-            return (string) $value;
+            $value = (string) $value;
         }
 
         if ($rule === 'slug') {
-            return $this->textToSlug($value, $this->data_rule->getAttribute('slug'));
+            $value = $this->textToSlug($value, $this->data_rule->getAttribute('slug'));
         }
 
         if ($rule === 'list' && $this->isJson($value)) {
-            return join(',', (array) json_decode($value));
+            $value = join(',', (array) json_decode($value));
         }
 
         if ($rule === 'date') {
             $date = \DateTime::createFromFormat($this->data_rule->getAttribute('from_format'), $value);
             if ($date) {
-                return $date->format($this->data_rule->getAttribute('to_format'));
+                $value = $date->format($this->data_rule->getAttribute('to_format'));
             }
         }
 
-        return $value;
+        if ($rule === 'strip_tags' && is_string($value)) {
+            $value = strip_tags($value,$this->data_rule->getAttribute('allowed_tags'));
+        }
+
+        if ($rule === 'replace_value') {
+            $value = $this->data_rule->getAttribute('replace_new_value') ;
+        }
+
+        if ($rule === 'replace_value_by_new') {
+            $value = $this->data_rule->getAttribute('replace_value_default') === $value ?  $this->data_rule->getAttribute('replace_value_new') : $value;
+        }
+
+        if ($rule === 'replace_text') {
+            $value = str_replace(
+                $this->data_rule->getAttribute('replace_text_default'),
+                $this->data_rule->getAttribute('replace_text_new'),
+                $value
+            );
+        }
+
+        $removed = null;
+
+        if ($rule === 'rename') {
+            $removed = $key;
+            $key = $this->data_rule->getAttribute('new_key_name_' . $key);
+        }
+
+        return (object) [
+            'key' =>  $key,
+            'value' => $value,
+            'remove' => $removed
+        ];
     }
 
     /**
@@ -133,19 +166,29 @@ class DataOptimizer
      */
     public function optimize($rules): array
     {
+        $data = [];
         call_user_func($rules, $this->data_rule);
         $this->data_rule->getRules();
 
-        if ($this->isMultipleRows($this->data)) {
+        if ((new DataValidator($this->data))->isArrayOfAssoc()) {
             foreach ($this->data as &$item) {
                 foreach ($item as $key => $value) {
                     if ($this->data_rule->hasRule($key)) {
-                        $item[$key] = $this->applyRule($this->data_rule->getRule($key), $value);
+
+                        $result =  $this->applyRule($this->data_rule->getRule($key), $key, $value);
+
+                        $value = $result->value;
+                        $key =  $result->key;
+                    }
+                    $item = array_merge($item, [$key => $value]);
+                    if(isset($result->remove)){
+                        unset( $item[$result->remove]);
                     }
                 }
+                $data[] = $item;
             }
         }
 
-        return $this->data;
+        return $data;
     }
 }
